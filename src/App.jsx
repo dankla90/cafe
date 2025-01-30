0be6c2c4beb1b5d5702f51e556e4c92afeca46ae
dankla90 <bassboom@hotmail.com>
2025-01-30 12:42:07 +0100

    Fixed the 404 issue
    The contacts page had a 404 error when refreshing the pages, I switched to browser router and added a script in the 404 page to fix this issue.

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,8 @@ const App = () => {
       <Header />
       <MainContent>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
         </Routes>
       </MainContent>
       <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    {/* Remove the basename for testing locally 
+      basename={process.env.PUBLIC_URL}
+    */}
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
